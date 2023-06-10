8d49291472b02b6c4ba26710035096bac25b5acf
sajeedazam <sajeedazam.20@gmail.com>
2023-06-09 19:59:51 -0700

    city box implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Categories from './components/Categories/categories';
 import About from './components/About/about';
+import City from './components/City/city';
 
 function App() {
   return (
     <div className="App">
-     <Login />
-     <Categories />
-     <About/>
+      <Login />
+      <City />
+      <Categories />
+      <About />
     </div>
   );
 }
