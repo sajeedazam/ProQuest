33a727ff7df9fcfdd628529977561531ded8eaa2
natester <sternbergnate@gmail.com>
2023-06-10 08:16:23 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from 'react';
 import './App.css';
 import Login from './components/Login/login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Categories from './components/Categories/categories';
 import About from './components/About/about';
 import City from './components/City/city';
+import SignupPage from './components/SignUp/signUp';
+import ProfessionalHomescreen from './components/pHomepage/phomepage.js'
+
+
+
+
+ 
 
 function App() {
   return (
     <div className="App">
       <Login />
+      <SignupPage />
       <City />
       <Categories />
       <About />
