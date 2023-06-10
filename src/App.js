df92915e9ece5dfda04d36cdfec4106938979825
Rahul <rahulkumarsinha5316@gmail.com>
2023-06-10 00:48:04 -0700

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Categories from './components/Categories/categories';
+//import About from './components/About/about';
+ import ProfessionalHomescreen from './components/pHomepage/phomepage.js'
+
+
+
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ProfessionalHomescreen/>
     </div>
   );
 }
