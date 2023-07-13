03cfbeb615758cf8a67f7b9341a80441cca10be5
Rahul <rahulkumarsinha5316@gmail.com>
2023-07-13 08:34:53 -0700

    Added models in server

--- client/src/components/Jobs/jobs.js
+++ client/src/components/Jobs/jobs.js
@@ -1,42 +1,98 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { acceptJob, rejectJob } from '../../redux/jobReducer';
+// import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { acceptJob, rejectJob } from '../../redux/jobReducer';
+// import { Link } from 'react-router-dom';
+// import '../Jobs/jobs.css';
+
+// function Jobs({ jobs }) {
+//     const dispatch = useDispatch();
+//     const [localJobs, setLocalJobs] = useState(jobs);
+
+//     const handleAcceptJob = (job) => {
+//       dispatch(acceptJob(job));
+//       setLocalJobs(localJobs.filter(j => j.name !== job.name));
+//       console.log(localJobs);
+//     };
+  
+//     const handleRejectJob = (job) => {
+//       dispatch(rejectJob(job));
+//       setLocalJobs(localJobs.filter(j => j.name !== job.name));
+//     };
+  
+//     if (!jobs || !Array.isArray(jobs)) {
+//       return <h2>No jobs available</h2>;
+//     }
+  
+//     return (
+//       <div >
+//         <Link to="/professional" className='mt-3'>Back</Link>
+//       <ul className="jobs-container"> 
+//         {localJobs.map((job, index) => (
+//           <div key={index} className="job-card">
+//             <p>{job.name}</p>
+//             <button onClick={() => handleAcceptJob(job)}>Accept</button>
+//             <button onClick={() => handleRejectJob(job)}>Reject</button>
+//           </div>
+//         ))}
+//       </ul>
+//       </div> 
+//     );
+//   }
+  
+//   export default Jobs;
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../Jobs/jobs.css';
 
-function Jobs({ jobs }) {
-    const dispatch = useDispatch();
-    const [localJobs, setLocalJobs] = useState(jobs);
+function Jobs() {
+    const [jobs, setJobs] = useState([]);
 
-    const handleAcceptJob = (job) => {
-      dispatch(acceptJob(job));
-      setLocalJobs(localJobs.filter(j => j.name !== job.name));
-      console.log(localJobs);
+    useEffect(() => {
+        fetchJobs();
+    }, []);
+
+    const fetchJobs = async () => {
+        const res = await axios.get('/jobs');
+        setJobs(res.data);
     };
-  
-    const handleRejectJob = (job) => {
-      dispatch(rejectJob(job));
-      setLocalJobs(localJobs.filter(j => j.name !== job.name));
+
+    const handleAcceptJob = async (job) => {
+        try {
+            await axios.delete(`/jobs/${job._id}`);
+            setJobs(jobs.filter(j => j._id !== job._id));
+        } catch (err) {
+            console.error(err);
+        }
     };
-  
+
+    const handleRejectJob = async (job) => {
+        try {
+            await axios.delete(`/jobs/${job._id}`);
+            setJobs(jobs.filter(j => j._id !== job._id));
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     if (!jobs || !Array.isArray(jobs)) {
-      return <h2>No jobs available</h2>;
+        return <h2>No jobs available</h2>;
     }
-  
+
     return (
-      <div >
-        <Link to="/professional" className='mt-3'>Back</Link>
-      <ul className="jobs-container"> 
-        {localJobs.map((job, index) => (
-          <div key={index} className="job-card">
-            <p>{job.name}</p>
-            <button onClick={() => handleAcceptJob(job)}>Accept</button>
-            <button onClick={() => handleRejectJob(job)}>Reject</button>
-          </div>
-        ))}
-      </ul>
-      </div> 
+        <div>
+            <Link to="/professional" className='mt-3'>Back</Link>
+            <ul className="jobs-container">
+                {jobs.map((job, index) => (
+                    <div key={index} className="job-card">
+                        <p>{job.name}</p>
+                        <button onClick={() => handleAcceptJob(job)}>Accept</button>
+                        <button onClick={() => handleRejectJob(job)}>Reject</button>
+                    </div>
+                ))}
+            </ul>
+        </div>
     );
-  }
-  
-  export default Jobs;
+}
+
+export default Jobs;
--- server/app.js
+++ server/app.js
@@ -1,20 +1,111 @@
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+// var app = express();
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// module.exports = app;
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Define the Job model here
+const Job = require('/Users/rahul/Desktop/cpsc455group/ProQuest/server/models/Job.js');
+
 var app = express();
 
+mongoose.connect('mongodb+srv://m001-student:m001-mongodb-basics@sandbox.r8nnfsp.mongodb.net/?retryWrites=true&w=majority', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define the /jobs endpoints here
+
+// GET endpoint to retrieve all jobs
+app.get('/jobs', async (req, res) => {
+  try {
+    const jobs = await Job.find({});
+    res.json(jobs);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// POST endpoint to add a new job
+app.post('/jobs', async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    let job = new Job({ name });
+    await job.save();
+    res.status(200).json({ msg: 'Job accepted successfully' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// PUT endpoint to update a job
+app.put('/jobs/:id', async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    let job = await Job.findByIdAndUpdate(req.params.id, { name }, { new: true });
+    res.status(200).json(job);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// DELETE endpoint to delete a job
+app.delete('/jobs/:id', async (req, res) => {
+  try {
+    await Job.findByIdAndRemove(req.params.id);
+    res.status(200).json({ msg: 'Job deleted successfully' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+var port = process.env.PORT || '5001';
+app.set('port', port);
+
+var server = app.listen(port, function () {
+  console.log(`Server is running on http://localhost:${port}`);
+});
