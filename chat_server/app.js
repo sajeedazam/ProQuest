37980634d25e7cdaf5c27919bba3d92f9f44652a
sajeedazam <sajeedazam.20@gmail.com>
2023-08-07 18:11:03 -0700

    cors options null

--- chat_server/app.js
+++ chat_server/app.js
@@ -10,13 +10,13 @@ require('dotenv').config();
 
 var app = express();
 var cors = require('cors');
-var http = require('http');
+// var http = require('http');
 
-const corsOptions = {
-    origin: "https://proquest.onrender.com",
-};
+// const corsOptions = {
+//     origin: "https://proquest.onrender.com",
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 app.use(logger('dev'));
@@ -26,58 +26,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// var server = http.createServer(app);
-// app.listen(PORT)
-// server.listen();
-// const io = require("socket.io")(server, {
-//     cors: {
-//       origin: "http://localhost:3000", // Update with the deployed frontend URL
-//       methods: ["GET", "POST"],
-//       allowedHeaders: ["my-custom-header"],
-//       credentials: true
-//     }
-//   });
-
-// const uri = process.env.MONGODB_CONNECTION_STRING;
-// mongoose.connect(`${uri}`, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-
-// var messageSchema = mongoose.Schema({
-//   user: String,
-//   message: String
-// });
-
-// var Message = mongoose.model('Message', messageSchema);
-
-//   io.on('connection', async function (socket) {
-//     console.log('client connected!')
-  
-//     const pastMessages = await Message.find({});
-//     socket.emit('pastMessages', pastMessages);
-  
-//     // Use process.nextTick to allow the 'pastMessages' event to be sent first
-//     process.nextTick(() => {
-//       socket.on('message', function (data) {
-//         console.log("Message from:", data.user, "Message:", data.message);
-  
-//         var newMessage = new Message({ user: data.user, message: data.message });
-      
-//         try {
-//           newMessage.save();
-//           console.log("Emit to all!");
-//           io.emit('message', data);  // Emit to all connected sockets
-//         } catch (error) {
-//           console.log(error);
-//         }
-//       });
-//     });
-  
-//   });
-
-//   app.get();
 
 module.exports = app;
